2f97fff4ba52ba0fdec2ea8c5a8e2a8e4a92ce29
Ramya <ramyarao@outlook.in>
2025-04-26 16:31:17 +0530

    Revised

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,10 +1,12 @@
 from src.pipeline import load_and_prepare_data, split_train_test
 from src.modelling import plot_roc_auc_curve, train_and_evaluate_model
 import pandas as pd
+import os
 
 def test_model():
-    data = load_and_prepare_data(file_path="tests\bank-full.csv",
-                                    target_column='y')
+    current_dir = os.getcwd()
+    csv_path = os.path.join(current_dir, 'tests', 'bank-full.csv')
+    data = load_and_prepare_data(file_path=csv_path, target_column='y')
     X_train, X_test, y_train, y_test = split_train_test(data=data, target_column='y')
     xgb_predictions, rf_predictions, xgb_accuracy, xgb_recall, rf_accuracy, rf_recall = train_and_evaluate_model(X_train, X_test, y_train, y_test)
 
